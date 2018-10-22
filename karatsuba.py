d8532335b3a67a9061b3d0afe47755f0fb5a6158
Skyler Penna <leveetoo@gmail.com>
2018-10-21 17:06:05 -0700

    close

--- karatsuba.py
+++ karatsuba.py
@@ -17,21 +17,27 @@ def preProcessLists(listOfInts1, listOfInts2):
     #make the larger of the two lists even then make the
     #smaller list of equal size
     if (len(listOfInts1) > len(listOfInts2)):
-        if len(listOfInts1) % 2 != 0:
+        if (len(listOfInts1) % 2) != 0:
             listOfInts1.insert(0, 0)
-        tmp = len(listOfInts1) - len(listOfInts2)
-        for i in range(1, tmp):
+        tmp = len(listOfInts1) - len(listOfInts2) + 1
+        while tmp > 0:
+            listOfInts2.insert(1, 0)
+            tmp = tmp - 1
+
+    elif(len(listOfInts1) < len(listOfInts2) ):
+        if (len(listOfInts2) % 2) != 0:
             listOfInts2.insert(0, 0)
-    else:
-        if len(listOfInts2) % 2 != 0:
-            listOfInts2. insert(0, 0)
-        tmp = len(listOfInts2) - len(listOfInts1)
-        for i in range(1, tmp):
+        tmp = len(listOfInts2) - len(listOfInts1) + 1
+        while tmp > 0:
             listOfInts1.insert(0, 0)
+            tmp = tmp - 1
+    else:
+        if (len(listOfInts1) % 2) != 0:
+            listOfInts1.insert(0, 0)
+            listOfInts2.insert(0, 0)
 
     return listOfInts1, listOfInts2
 
-    #karatsuba(listOfInts1, listOfInts2)
 
 #splits a list of even size in half and returns the two lists
 def splitList(listofInts):
@@ -44,23 +50,32 @@ def splitList(listofInts):
 def addLists(list1, list2):
     tmp = []
     carry = 0
+    list1, list2 = preProcessLists(list1, list2)
+    i = (len(list1)-1)
 
-    i = len(list1)-1
-    j = len(list2)-1
-
-    while i > -1 and j > -1:
-        if ((list1[i] + list2[j] + carry) > 9):
-            tmp.insert(0, 0)
+    while i > 0:
+        if ((int(list1[i]) + int(list2[i]) + carry) > 9):
+            sumElem = int(list1[i]) + int(list2[i]) + carry
+            sumElem = sumElem % 10
+            tmp.insert(0, sumElem)
             carry = 1
         else:
-            n = list1[i] + list2[j] + carry
+            n = int(list1[i]) + int(list2[i]) + carry
             tmp.insert(0, n)
             carry = 0
         i = i-1
-        j = j-1
 
-    n = list1[i]+carry
-    tmp.insert(0, n)
+    if ((int(list1[i]) + int(list2[i]) + carry) > 9):
+        sumElem = int(list1[i]) + int(list2[i]) + carry
+        sumElem = sumElem % 10
+        tmp.insert(0, sumElem)
+        carry = 1
+    else:
+        n = int(list1[i]) + int(list2[i]) + carry
+        tmp.insert(0, n)
+        carry = 0
+    if carry > 0:
+        tmp.insert(0, carry)
 
     return tmp
 
@@ -69,25 +84,26 @@ def addLists(list1, list2):
 def subLists(list1, list2):
     tmp = []
     carry = 0
+    list1, list2 = preProcessLists(list1, list2)
+    i = (len(list1)-1)
 
-    i = len(list1)-1
-    j = len(list2)-1
-
-    while i > -1 and j > -1:
-        if ((list1[i] - list2[j]) < 0):
-            tmp.insert(0, ((10+list1[i])-list2[j]))
+    while i > -1:
+        if (int(list1[i]) - int(list2[i])) < 0:
+            tmp.insert(0, ((10+int(list1[i]))-int(list2[i]) - carry))
             carry = 1
         else:
-            tmp.insert(0, (list1[i] - list2[j] - carry))
+            tmp.insert(0, (int(list1[i]) - int(list2[i]) - carry))
+            carry = 0
         i = i-1
-        j = j-1
+
+
 
     return tmp
 
 
 
 def karatsuba(listOfInts1, listOfInts2):
-    if (len(listOfInts1) == 1 and len(listOfInts2) == 1):
+    if len(listOfInts1) == 1 or len(listOfInts2) == 1:
         tmp = []
         tint1 = "".join(map(str, listOfInts1))
         tint2 = "".join(map(str, listOfInts2))
@@ -96,7 +112,10 @@ def karatsuba(listOfInts1, listOfInts2):
         for i in tint1:
             tmp.append(i)
         return tmp
+
     else:
+
+        listOfInts1, listOfInts2 = preProcessLists(listOfInts1, listOfInts2)
         num = len(listOfInts1)/2
 
         l1First, l1Second = splitList(listOfInts1)
@@ -105,15 +124,17 @@ def karatsuba(listOfInts1, listOfInts2):
         c2 = karatsuba(l1First, l2First)
         c0 = karatsuba(l1Second, l2Second)
 
-        z0 = addLists(l1First, l2First)
-        z1 = addLists(l1Second, l2Second)
+        fHalf = addLists(l1First, l2First)
+        sHalf = addLists(l1Second, l2Second)
 
-        z3 = karatsuba(z0, z1)
+        z3 = karatsuba(fHalf, sHalf)
+
         z2 = addLists(c2, c0)
 
         c1 = subLists(z3, z2)
 
-        shift = 2*num
+        shift = num + num
+
         for i in range(1, shift):
             c2.append(0)
         for x in range(1, num):
@@ -121,9 +142,9 @@ def karatsuba(listOfInts1, listOfInts2):
 
         c3 = addLists(c2, c1)
 
-        c3 = addLists(c3, c0)
+        result = addLists(c3, c0)
 
-        return c3
+        return result
 
 
 
@@ -147,7 +168,6 @@ def main():
             num2 = input("Enter second number: ")
             list1 = convertIntToList(num1)
             list2 = convertIntToList(num2)
-            list1, list2 = preProcessLists(list1, list2)
             result = karatsuba(list1, list2)
             print(result)
 
